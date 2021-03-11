43b75d68688b934821b750f7738e37e98f9ccf97
veeresh <veereshsoni913@gmail.com>
2021-03-11 17:52:03 +0530

    adds some smiles

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 def git_opeation():
-    #adding some sample comments
-    #not a big change I know 
+    #adding some sample comments:)
+    #not a big change I know :)
  print("I am adding example.py file to the remote repository.")
 git_opeation()
